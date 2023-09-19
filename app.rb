8b0f118931fbc929a104b7fcc05fee75e5eb7d95
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-09-19 22:51:27 +0000

    subtraction working!

--- app.rb
+++ app.rb
@@ -34,7 +34,7 @@ get("/wizard_subtract") do
 
   @second_num = params.fetch("second_num").to_f
 
-  @result = first_num + @second_num
+  @result = @first_num + @second_num
 
   erb(:sub_result)
 end
