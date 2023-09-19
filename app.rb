b3baad4290772d73dae0982fd08e3e7a07731755
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-09-19 23:02:50 +0000

    multiplication working!

--- app.rb
+++ app.rb
@@ -44,6 +44,12 @@ get("/multiply") do
 end
 
 get("/wizard_multiply") do
+  @first_number = params.fetch("first_num").to_f
+
+  @second_number = params.fetch("second_num").to_f
+
+  @result = @first_number * @second_number
+
   erb(:mult_result)
 end
 
